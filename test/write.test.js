48ab43b974419eacc2b65fb1e19d393acde84687
Colin <why101@msn.com>
2018-08-19 23:50:30 -0700

    expanded test to test multiple scripts

--- test/write.test.js
+++ test/write.test.js
@@ -1,28 +1,47 @@
 let path = require('path'),
     fs = require('fs'),
+    ffp = require('ffp'),
     {PexFile} = require('../dist/index');
 
-let inputPath = path.resolve(__dirname, './fixtures/game.pex'),
-    outputPath = path.resolve(__dirname, './output/game.pex');
+let scriptsPath = path.resolve(__dirname, './scripts'),
+    outputPath = path.resolve(__dirname, './output');
+
+ffp.setLogger({
+    log: () => {},
+    warn: () => {},
+    error: console.error
+});
+
+let testFile = function(filename, done) {
+    let scriptPath = path.resolve(scriptsPath, filename),
+        copyPath = path.resolve(outputPath, filename),
+        script = new PexFile(scriptPath);
+    script.parse(err => {
+        expect(err).toBeUndefined();
+        script.filePath = copyPath;
+        script.write(err => {
+            expect(err).toBeUndefined();
+            let input = fs.readFileSync(scriptPath),
+                output = fs.readFileSync(copyPath);
+            expect(input).toBeDefined();
+            expect(output).toBeDefined();
+            expect(Buffer.isBuffer(input)).toBe(true);
+            expect(Buffer.isBuffer(output)).toBe(true);
+            expect(input.length).toBe(output.length);
+            expect(input).toEqual(output);
+            console.log(`Verified ${filename}`);
+            done();
+        });
+    });
+};
 
 describe('Pex File Writing', () => {
-    it('should yield a binary identical file', done => {
-        let script = new PexFile(inputPath);
-        script.parse(err => {
-            expect(err).toBeUndefined();
-            script.filePath = outputPath;
-            script.write(err => {
-                expect(err).toBeUndefined();
-                let input = fs.readFileSync(inputPath),
-                    output = fs.readFileSync(outputPath);
-                expect(input).toBeDefined();
-                expect(output).toBeDefined();
-                expect(Buffer.isBuffer(input)).toBe(true);
-                expect(Buffer.isBuffer(output)).toBe(true);
-                expect(input.length).toBe(output.length);
-                expect(input).toEqual(output);
-                done();
+    describe('binary identical', () => {
+        let files = fs.readdirSync(scriptsPath, {});
+        files.forEach(file => {
+            it(`should be binary identical, ${file}`, done => {
+                testFile(file, done);
             });
-        });
+        })
     });
 });
